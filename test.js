4b06a78deebb80ce9e201a02d5a4f17a73d2a6ce
Henry Campos <henry.campos@galileo.edu>
2017-07-15 12:04:44 -0600

    commit 2

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
+//*nmcli c down <network> && nmcli c up <network> && node test.js
+
 var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 var fs = require('fs');
 var Sound = require('node-aplay');
-var readline = require('readline')
+var readline = require('readline');
 
-//! sad
+//!Comment
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -22,14 +24,14 @@ var read = rl.question('Write something cool: ', (answer) => {
   };
   text_to_speech.synthesize(params).on('finish', (error) => {
     console.log('Error:', error);
-  }).pipe( fs.createWriteStream('hello_world.wav').on('finish', () => {
-              var music = new Sound('hello_world.wav').play();
-          }) );
+  }).pipe(fs.createWriteStream('answer.wav').on('finish', () => {
+              var music = new Sound('answer.wav').play( () => {
+                try{
+                  fs.unlinkSync('answer.wav', () => {
+                  
+                  });
+                }catch(error){}
+              });
+          }));
   rl.close();
 });
-
-
-
-
-
-// Pipe the synthesized text to a file.
